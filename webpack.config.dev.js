d25ba559dc7cca4967de3a7cbbbacff6a5ea394d
khs <noisonnoiton@gmail.com>
2020-06-16 14:49:13 +0900

    backup

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -18,12 +18,12 @@ module.exports = {
         contentBase: __dirname + '/dist/',
         proxy: {
           '/nice': {
-            target: 'http://192.168.0.8:8081', //nice
+            target: 'http://192.168.0.13:8081', //nice
                 secure: false,
                 changeOrigin: true,
            },
           '/': {
-              target: 'http://192.168.0.8:3007', //backend api (bp사)
+              target: 'http://192.168.0.13:3007', //backend api (bp사)
                   secure: false,
                   changeOrigin: true,
           }
